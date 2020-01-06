8ef81cb48b4c42725498c21c079b2e2a7738462c
Anthony Asp <asp.anthony1@gmail.com>
2020-01-05 17:39:08 -0800

    remove commented world init functions

--- main.py
+++ main.py
@@ -187,38 +187,5 @@ the world gave up looking for places to add food.""".format(
                 for row in world_array]
 
 
-    #
-    # def init_loaded_world(self, init_world_array):
-    #     world_cells = []
-    #     for col_pos in range(0, self.world_height - 1):
-    #         world_row = []
-    #         for row_pos in range(0, self.world_width - 1):
-    #             cell = new_cell(row_pos, col_pos)
-    #             loadup_value = init_world_array[col_pos][row_pos]
-    #             if loadup_value:
-    #                 birth(cell)
-    #             world_row.append(cell)
-    #         world_cells.append(world_row)
-    #     return world_cells
-    #
-    # def init_fresh_world(self):
-    #     world_cells = []
-    #     for col_pos in range(0, self.world_height - 1):
-    #         world_row = []
-    #         for row_pos in range(0, self.world_width - 1):
-    #             cell = new_cell(row_pos, col_pos)
-    #             if bool(random.randint(0, 1)):
-    #                 birth(cell)
-    #             world_row.append(cell)
-    #         world_cells.append(world_row)
-    #     return world_cells
-    #
-    # def init_dead_world(self):
-    #     dead_world = [[new_cell(row_pos, col_pos)
-    #                    for row_pos in range(0, self.world_width - 1)]
-    #                   for col_pos in range(0, self.world_height - 1)]
-    #     return dead_world
-
-
 if __name__ == "__main__":
     game = Game()
