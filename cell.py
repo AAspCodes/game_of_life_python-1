24ac7a32160df9349ecee096ed79ee8021c7b554
Anthony Asp <asp.anthony1@gmail.com>
2020-01-05 16:45:57 -0800

    add cell functions to cell.py
    + new_cell()
    + kill_cell()
    + birth_cell()
    + evaluate_cell()

--- cell.py
+++ cell.py
@@ -1,1 +1,30 @@
 
+def new_cell(x_value, y_value):
+    return {
+        'alive': False,
+        'x-value': x_value,
+        'y-value': y_value,
+        'alive-neighbor-count': 0,
+        'has-food': False,
+        'neighbor-has-food': False
+    }
+
+
+def kill(cell):
+    cell['alive'] = False
+
+
+def birth(cell):
+    cell['alive'] = True
+
+
+def evaluate_cell(cell):
+    neighbors = cell['alive-neighbor-count']
+    neighbors_have_food = cell['neighbor-has-food']
+    if cell['alive']:
+        if neighbors not in [2, 3]:
+            kill(cell)
+    else:
+        if neighbors == 3 or neighbors_have_food:
+            birth(cell)
+    cell['has-food'] = False
